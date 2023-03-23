fd6e677848fb97534558d05fa312bb2fd2a32241
flo456123 <zachappella@gmail.com>
2023-03-22 22:30:41 -0400

    fixed path issue to .env file

--- src/main/java/me/flo456123/FriendlyBot/Config.java
+++ src/main/java/me/flo456123/FriendlyBot/Config.java
@@ -3,7 +3,7 @@ package me.flo456123.FriendlyBot;
 import io.github.cdimascio.dotenv.Dotenv;
 
 public class Config {
-    private static final Dotenv dotenv = Dotenv.configure().directory("C:\\Users\\zacha\\IdeaProjects\\Friendly Bot\\.env").load();
+    private static final Dotenv dotenv = Dotenv.configure().directory("./.env").load();
 
     public static String get(String key) {
         return dotenv.get(key);
