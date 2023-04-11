245dcf56350d0e7cde0aa766811dc0fd6313ca52
flo456123 <zachappella@gmail.com>
2023-04-11 16:53:02 -0400

    increased bufferSize

--- src/main/java/net/clf/jda/config/Config.java
+++ src/main/java/net/clf/jda/config/Config.java
@@ -1,7 +1,7 @@
 package net.clf.jda.config;
 
 public class Config {
-    private static int bufferSize = 200;
+    private static int bufferSize = 2000;
 
     public static void setBufferSize(int size) {
         bufferSize = size;
