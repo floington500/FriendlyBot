1648efe4ae1e3a844ed91b4cf1f5dca19b0b5561
flo456123 <zachappella@gmail.com>
2023-04-11 21:50:22 -0400

    small changes: - changed path to env file in `Environment.java` - improved project buildpath

--- src/main/java/net/clf/jda/config/Environment.java
+++ src/main/java/net/clf/jda/config/Environment.java
@@ -6,7 +6,7 @@ import io.github.cdimascio.dotenv.Dotenv;
  * The {@link Environment} class provides access to environment variables stored in a .env file located in the application's root directory.
  */
 public class Environment {
-    private static final Dotenv dotenv = Dotenv.configure().directory("./.env").load();
+    private static final Dotenv dotenv = Dotenv.configure().directory(".env").load();
 
     /**
      * Retrieves the value of the environment variable with the specified key.
